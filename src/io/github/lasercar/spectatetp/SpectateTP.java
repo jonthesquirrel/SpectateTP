c78fb5d5d2dccfb06f3151b2ce1bf9cbabc892bc
Lasercar7 <lasercar@users.noreply.github.com>
2015-09-08 00:07:41 -0500

    create plugin

---
+++ src/io/github/lasercar/spectatetp/SpectateTP.java
@@ -0,0 +1,37 @@
+package io.github.lasercar.spectatetp;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpectateTP extends JavaPlugin implements Listener {
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+    }
+
+    public String colorize(String string) {
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
+
+    @EventHandler
+    public void onTp(PlayerTeleportEvent event) {
+
+        Player player = event.getPlayer();
+        PlayerTeleportEvent.TeleportCause cause = event.getCause();
+        String deny_message = colorize(this.getConfig().getString("deny-message"));
+
+        if (cause.toString().equals("SPECTATE")) {
+            if (!player.hasPermission("spectatetp.tp")) {
+                event.setCancelled(true);
+                player.sendMessage(deny_message);
+            }
+        }
+
+    }
+
+}
