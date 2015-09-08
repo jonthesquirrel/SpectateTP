a795a87e637048676f9d8f3905a65ae474098c75
Lasercar7 <lasercar@users.noreply.github.com>
2015-09-08 01:53:05 -0500

    built jar properly to reduce size dramatically

--- src/io/github/lasercar/spectatetp/SpectateTP.java
+++ src/io/github/lasercar/spectatetp/SpectateTP.java
@@ -10,8 +10,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class SpectateTP extends JavaPlugin implements Listener {
 
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
         saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     public String colorize(String string) {
